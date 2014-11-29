79b5d8b716b3048426a9246286d8b2fb2b16d55c
Lorless <dannylaws@hotmail.co.uk>
2014-11-29 21:36:04 +0000

    OpenShift Text

--- server.js
+++ server.js
@@ -192,6 +192,7 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 
+// OPENSHIFT PICKUP CHANGE
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.listen( port, ipaddress, function() {
