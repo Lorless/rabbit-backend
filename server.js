175b73ae30f3594ce8740230821d43603e77b74d
Lorless <dannylaws@hotmail.co.uk>
2014-11-29 21:22:58 +0000

    OpenShift Fix
    Added ip and port listener for openshift

--- server.js
+++ server.js
@@ -191,5 +191,13 @@ app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Server is running on port: ' + port);
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+app.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+
+// app.listen(port);
+// console.log('Server is running on port: ' + port);
